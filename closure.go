e9b0b8734167a60ef9440be06c919f34b9edf575
Tommy <thomz161@gmail.com>
2023-06-23 18:01:37 +0700

    updated

---
+++ closure.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	counter := 0
+
+	increment := func() {
+		fmt.Println("Increment")
+		counter++
+	}
+
+	increment()
+	increment()
+	increment()
+
+	fmt.Println(counter)
+
+}
